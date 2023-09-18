bf5bdb267910926ee069f6189fd549ddfa72b77c
Lucas Lima <lucas.sdslima@gmail.com>
2023-09-18 14:17:46 -0300

    test: home.service should delete all players

--- src/app/home/home.service.spec.ts
+++ src/app/home/home.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HomeService } from './home.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('HomeService', () => {
   let service: HomeService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HomeService],
+    });
     service = TestBed.inject(HomeService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should delete all teams and players', () => {
+    const url = 'http://localhost:8080/jogador/all';
+
+    const mockResponse = {};
+
+    service.deleteAll().subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(url);
+
+    expect(req.request.method).toBe('DELETE');
+
+    req.flush(mockResponse);
   });
 });
